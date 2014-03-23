e5cc5d32d16f10911ab38ffe3f2b1f854e154656
Rongduan Zhu <rongduan.zhu@gmail.com>
2014-03-24 00:41:39 +1100

    partially started skeleton code

---
+++ src/Board.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+
+public class Board {
+	private ArrayList<ArrayList<Cell> > cellArray;
+	
+	Board(int dimension) {
+		/* Specifying capacity at start will save the time of dynamically reallocating
+		 * more memory */
+		dimension = dimension * 2 - 1;
+		cellArray = new ArrayList<ArrayList<Cell>>(dimension * 2 - 1);
+		for (int i = 0; i < dimension; ++i) {
+			cellArray.add(new ArrayList<Cell>());
+//			todo: allocate memory for each cell
+		}
+	} 
+}
