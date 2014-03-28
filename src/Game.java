52da5aa7ea5695955b31b80d01b6d99b6652e2bc
Rongduan Zhu <rongduan.zhu@gmail.com>
2014-03-29 03:19:44 +1100

    added input reader

--- src/Game.java
+++ src/Game.java
@@ -7,12 +7,21 @@ public class Game {
 	/* Temporary main method in here for testing */
 	public static void main(String args[]) {
 		Board board = new Board(5);
-		readStdInput();		
+		readStdInput(board);		
 		System.out.println(board);
 	}
 	
 	public static void readStdInput(Board board) {
 		Scanner stdin = new Scanner(System.in);
-		int size = stdin.nextInt();
+		int size = stdin.nextInt() * 2 - 1;
+		String item = new String();
+		for (int i = 0; i < size; ++i) {
+			for (int j = 0; j < size; ++j) {
+				if (board.isValidPosition(i, j)) {
+					item = stdin.next();
+					board.set(i, j, item);
+				}
+			}
+		}
 	}
 }
