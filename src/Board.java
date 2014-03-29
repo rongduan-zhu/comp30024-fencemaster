99eece905096260c7ac6c11b559604abc0f3dda5
Rongduan Zhu <rongduan.zhu@gmail.com>
2014-03-29 18:56:49 +1100

    made new method for getting number of columns for a specific row

--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,7 @@ public class Board {
 	private ArrayList<ArrayList<Cell> > cellArray;
 	private Integer dimension;
 
+
 	public Board(int dimension) {
 		/* Specifying capacity at start will save the time of dynamically reallocating
 		 * more memory */
@@ -19,32 +20,28 @@ public class Board {
 			}
 		}
 	}
-	
+
 	public boolean isValidPosition(int row, int column) {
 		if (row < 0 || row > this.dimension - 1 ||
 				column < 0 || column > this.dimension - 1) {
 			return false;
 		}
-		
-		int boardMiddle = (this.dimension + 1) / 2 - 1,
-			columnBound = boardMiddle + row;
-		if (row > boardMiddle) {
-			columnBound = boardMiddle + (this.dimension - 1) - row;
-		}
-		
+
+		int columnBound = getMaxColumn(row);
+
 		if (column <= columnBound) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public String get(int row, int column) {
 		if (isValidPosition(row, column)) {
 			return cellArray.get(row).get(column).getContent();
 		}
 		return "";
 	}
-	
+
 	public boolean set(int row, int column, String type) {
 		if (!isValidPosition(row, column)) {
 			return false;
@@ -52,7 +49,7 @@ public class Board {
 		cellArray.get(row).get(column).setContent(type);
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
 		String boardString = new String();
@@ -63,5 +60,21 @@ public class Board {
 			boardString += "\n";
 		}
 		return boardString;
+	}
+
+	public int getMaxColumn(int row) {
+		if (row > this.dimension - 1) {
+			return -1;
+		}
+		int boardMiddle = (this.dimension + 1) / 2 - 1,
+			columnBound = boardMiddle + row;
+		if (row > boardMiddle) {
+			columnBound = boardMiddle + (this.dimension - 1) - row;
+		}
+		return columnBound;
+	}
+
+	public String getNeighbour(int row, int column) {
+		return "";
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
-import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
-
-
 public class Game {
 	/* Temporary main method in here for testing */
 	public static void main(String args[]) {
 		Board board = new Board(5);
-		readStdInput(board);		
+		readStdInput(board);
 		System.out.println(board);
 	}
-	
+
 	public static void readStdInput(Board board) {
 		Scanner stdin = new Scanner(System.in);
 		int size = stdin.nextInt() * 2 - 1;
